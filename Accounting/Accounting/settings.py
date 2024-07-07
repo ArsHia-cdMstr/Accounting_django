f04387a32bf355a346cd4813a1c87123ac4bd502
arshia.mkh <arshia.mokhlesi@gmail.com>
2024-07-07 10:20:00 +0330

    complete login , signup , logout , portfolio list

--- Accounting/Accounting/settings.py
+++ Accounting/Accounting/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
@@ -86,18 +86,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {     # Note: remove the limits for passwords
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
--- Accounting/app/urls.py
+++ Accounting/app/urls.py
@@ -1,5 +1,10 @@
+from .views import UserSignup, UserLogin, PortfolioList
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 urlpatterns = [
-    
+    path('signup/', UserSignup.as_view(), name='signup'),
+    path('login/', UserLogin.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('', PortfolioList.as_view(), name='pfl-list'),
 ]
